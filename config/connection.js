083d2ef7aef3ada883e7c152c537999605456907
Stephen Woo <stephenwoo95@gmail.com>
2017-08-26 02:00:45 -0700

    wrote most files

--- config/connection.js
+++ config/connection.js
@@ -11,3 +11,13 @@ if(process.env.JAWSDB_URL) {
     database: 'burgers_DB'
   });
 }
+
+connection.connect(function(err) {
+  if(err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+})
+
+module.exports = connection;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var burger = require('../models/burger.js');
+
+var router = express.Router();
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burger.insertOne("burger_name",
+    req.body.burgerName, function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
